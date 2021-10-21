ca7e230d1a46ac953117f3950a2f49d0cc7b8c74
Philipp Zahn <philipp.zahn@protonmail.com>
2021-10-21 16:44:36 +0200

    Corrected error learning

--- src/Examples/QLearning/AsymmetricLearners/InternalThreePhases.hs
+++ src/Examples/QLearning/AsymmetricLearners/InternalThreePhases.hs
@@ -561,7 +561,7 @@ executeAndRematchSingleRun :: String
 executeAndRematchSingleRun name  exportConfigGameLearning parametersMap keepOnlyNLastIterations parametersGameRematchingMapPhase2 parametersGameRematchingMapPhase3 exportConfigGameRematchingPhase2 exportConfigGameRematchingPhase3 expIds rematchTypeIdsPhase2 rematchTypeIdsPhase3= do
   qTablesMapPhase1 <- mapM (firstStageLearningMap ("_phase1_run_" ++ name)  keepOnlyNLastIterations exportConfigGameLearning parametersMap) expIds
   let aggMap1Phase1 = unions $ fmap fst qTablesMapPhase1
-      aggMap2Phase2 = unions $ fmap snd qTablesMapPhase1
+      aggMap2Phase1 = unions $ fmap snd qTablesMapPhase1
   mapM_ (rematchedLearning ("_phase2_run_" ++ name) keepOnlyNLastIterations parametersGameRematchingMapPhase2 exportConfigGameRematchingPhase2 (aggMap1Phase1,aggMap2Phase1)) rematchTypeIdsPhase2
   mapM_ (rematchedLearning ("_phase3_run_" ++ name) keepOnlyNLastIterations parametersGameRematchingMapPhase3 exportConfigGameRematchingPhase3 (aggMap1Phase1,aggMap2Phase1)) rematchTypeIdsPhase3
 
