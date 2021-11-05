42528c3ccf005c6015af46d8d5606501eee17bf6
Philipp Zahn <philipp.zahn@protonmail.com>
2021-11-05 16:15:26 +0100

    Added 3Phases

---
+++ src/Examples/QLearning/AsymmetricLearners3Phases.hs
@@ -0,0 +1,34 @@
+module Examples.QLearning.AsymmetricLearners3Phases
+  ( initialStrat
+  , initialArray1
+  , initialArray2
+  , randomInitialObservation
+  , actionSpace1
+  , actionSpace2
+  , csvParameters
+  , sequenceL
+  , evalStageM
+  , mapStagesM_
+  , mapStagesMFinalResult
+  , firstStageLearningMap
+  , rematchedLearning
+  , rematchedLearningWithName
+  , executeAndRematchSingleRun
+  , PriceSpace(..)
+  , Observation(..)
+  , Parameters(..)
+  , ReMatchType(..)
+  ) where
+
+import           Examples.QLearning.AsymmetricLearners.Internal
+import           Data.HashMap
+
+-- Model for paper: three cost levels
+{-# INLINE executeAndRematchSingleRun #-}
+executeAndRematchSingleRun runNo exportConfigGameLearning parametersMap keepOnlyNLastIterations parametersGameRematchingMapPhase2 parametersGameRematchingMapPhase3 exportConfigGameRematchingPhase2 exportConfigGameRematchingPhase3 expIds rematchTypeIdsPhase2 rematchTypeIdsPhase3= do
+          qTablesMapPhase1 <- mapM (firstStageLearningMap ("_phase1_run_" ++ name) runNo  keepOnlyNLastIterations exportConfigGameLearning parametersMap) expIds
+          let aggMap1Phase1 = unions $ fmap fst qTablesMapPhase1
+              aggMap2Phase1 = unions $ fmap snd qTablesMapPhase1
+          mapM_ (rematchedLearning ("_phase2_run_" ++ name) runNo keepOnlyNLastIterations parametersGameRematchingMapPhase2 exportConfigGameRematchingPhase2 (aggMap1Phase1,aggMap2Phase1)) rematchTypeIdsPhase2
+          mapM_ (rematchedLearning ("_phase3_run_" ++ name) runNo keepOnlyNLastIterations parametersGameRematchingMapPhase3 exportConfigGameRematchingPhase3 (aggMap1Phase1,aggMap2Phase1)) rematchTypeIdsPhase3
+  where name = show runNo
