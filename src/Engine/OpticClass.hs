945c02b5ec3080ffa0eb51bfccb751871ca7638e
Philipp Zahn <philipp.zahn@protonmail.com>
2022-01-26 09:13:00 +0100

    Integrating markov stuff

--- src/Engine/OpticClass.hs
+++ src/Engine/OpticClass.hs
@@ -13,6 +13,8 @@ module Engine.OpticClass
   , MonadContext(..)
   , MonadicLearnLens(..)
   , MonadicLearnLensContext(..)
+  , MonadOpticMarkov(..)
+  , MonadContextMarkov(..)
   , Optic(..)
   , Precontext(..)
   , Context(..)
@@ -22,9 +24,8 @@ module Engine.OpticClass
 
 
 import           Control.Monad.State                hiding (state)
+import           Data.HashMap                       as HM hiding (null,map,mapMaybe)
 import           Numeric.Probability.Distribution   hiding (lift)
-
-
 
 class Optic o where
   lens :: (s -> a) -> (s -> b -> t) -> o s t a b
@@ -293,3 +294,46 @@ instance Monad m => Context (MonadicLearnLensContext m) (MonadicLearnLens m) whe
                                             (b1,b2) <- k'' (a1,a2)
                                             pure b1) 
                   in MonadicLearnLensContext h' k'
+
+-------------------------------------------------------------
+-- 5 Implementation for Markov games
+
+data MonadOpticMarkov s t a b where
+  MonadOpticMarkov :: (s -> IO (z, a))
+                          -> (z -> b -> StateT Vector IO t)
+                          -> MonadOpticMarkov s t a b
+
+instance Optic MonadOpticMarkov where
+  lens v u = MonadOpticMarkov (\s -> return (s, v s)) (\s b -> return (u s b))
+  (>>>>) (MonadOpticMarkov v1 u1) (MonadOpticMarkov v2 u2) = MonadOpticMarkov v u
+    where v s = do {(z1, a) <- v1 s; (z2, p) <- v2 a; return ((z1, z2), p)}
+          u (z1, z2) q = do {b <- u2 z2 q; u1 z1 b}
+  (&&&&) (MonadOpticMarkov v1 u1) (MonadOpticMarkov v2 u2) = MonadOpticMarkov v u
+    where v (s1, s2) = do {(z1, a1) <- v1 s1; (z2, a2) <- v2 s2; return ((z1, z2), (a1, a2))}
+          u (z1, z2) (b1, b2) = do {t1 <- u1 z1 b1; t2 <- u2 z2 b2; return (t1, t2)}
+  (++++) (MonadOpticMarkov v1 u1) (MonadOpticMarkov v2 u2) = MonadOpticMarkov v u
+    where v (Left s1)  = do {(z1, a1) <- v1 s1; return (Left z1, Left a1)}
+          v (Right s2) = do {(z2, a2) <- v2 s2; return (Right z2, Right a2)}
+          u (Left z1) b  = u1 z1 b
+          u (Right z2) b = u2 z2 b
+
+data MonadContextMarkov s t a b where
+  MonadContextMarkov :: (Show z) => IO (z, s) -> (z -> a -> StateT Vector IO b) -> MonadContextMarkov s t a b
+
+instance Precontext MonadContextMarkov where
+  void = MonadContextMarkov (return ((), ())) (\() () -> return ())
+
+instance Context MonadContextMarkov MonadOpticMarkov where
+  cmap (MonadOpticMarkov v1 u1) (MonadOpticMarkov v2 u2) (MonadContextMarkov h k)
+            = let h' = do {(z, s) <- h; (_, s') <- v1 s; return (z, s')}
+                  k' z a = do {(z', a') <- lift (v2 a); b' <- k z a'; u2 z' b'}
+               in MonadContextMarkov h' k'
+  (//) (MonadOpticMarkov v u) (MonadContextMarkov h k)
+            = let h' = do {(z, (s1, s2)) <- h; return ((z, s1), s2)}
+                  k' (z, s1) a2 = do {(_, a1) <- lift (v s1); (_, b2) <- k z (a1, a2); return b2}
+               in MonadContextMarkov h' k'
+  (\\) (MonadOpticMarkov v u) (MonadContextMarkov h k)
+            = let h' = do {(z, (s1, s2)) <- h; return ((z, s2), s1)}
+                  k' (z, s2) a1 = do {(_, a2) <- lift (v s2); (b1, _) <- k z (a1, a2); return b1}
+               in MonadContextMarkov h' k'
+
