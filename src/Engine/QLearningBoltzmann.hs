7cd0920309b0c3dda8e799f04751d23452402449
Philipp Zahn <philipp.zahn@protonmail.com>
2022-01-27 17:50:49 +0100

    Checking on value problem due to precision

--- src/Engine/QLearningBoltzmann.hs
+++ src/Engine/QLearningBoltzmann.hs
@@ -588,7 +588,11 @@ boltzmannCTable  exploreRate qTable0 obs cTable0 = do
                   value <- A.readArray qTable0 index
                   pure (action,value))
              (population cTable0))
-  let actionValue = \(action,value) -> (action, ((exp 1.0) ** (value / exploreRate)))
+  let actionValue =
+        let newValue = ((exp 1.0) ** (value / exploreRate))
+            in \(action,value) ->
+                 if newValue > 0.000001 then (action, ((exp 1.0) ** (value / exploreRate)))
+                                        else  (action, 0.000001)
       denominator = sum (fmap (snd . actionValue) ls)
       updateProbability = \(action,value) -> (action,(((exp 1.0) ** (value / exploreRate)) / denominator))
       newProbabilityV   = tableFromProbabilities $  fmap updateProbability ls
