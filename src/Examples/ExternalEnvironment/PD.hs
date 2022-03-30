999434ae7407196d6921ecd1901038331db30c0f
silky <noonsilk+-noonsilk@gmail.com>
2022-03-30 09:19:07 +0100

    Add servant API
    - Demonstrate calling it
    - Running play, returning payoffs!

--- src/Examples/ExternalEnvironment/PD.hs
+++ src/Examples/ExternalEnvironment/PD.hs
@@ -1,6 +1,15 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
 
 module Examples.ExternalEnvironment.PD where
 
@@ -11,7 +20,96 @@ import Preprocessor.Preprocessor
 import Engine.ExternalEnvironment
 import Examples.SimultaneousMoves (prisonersDilemmaMatrix,ActionPD(..))
 
+-- TODO: Explicit imports!
+import Servant.API
+import Servant
+import Network.Wai.Handler.Warp (setPort, setBeforeMainLoop, runSettings, defaultSettings)
+import Data.Aeson
+import GHC.Generics
+import Control.Monad.IO.Class (liftIO)
 
+
+data PlayParameters = PlayParameters
+  { player1Action :: ActionPD
+  , player2Action :: ActionPD
+  } deriving (Show, Generic, ToJSON, FromJSON)
+
+data PlayResult = PlayResult
+  { player1Payoff :: Double
+  , player2Payoff :: Double
+  } deriving (Show, Generic, ToJSON, FromJSON)
+
+-- Using: https://httpie.io/docs/cli/json
+--
+--  http localhost:3000/healthcheck
+--  http POST localhost:3000/play player1Action=Cooperate player2Action=Cooperate
+--
+type Api = "play" :> ReqBody '[JSON] PlayParameters :> Post '[JSON] PlayResult
+           :<|> "healthcheck" :> Get '[JSON] String
+
+api :: Proxy Api
+api = Proxy
+
+server :: Server Api
+server =
+  runPlay
+  :<|> return "Ok!"
+
+run :: IO ()
+run = do
+  let port = 3000
+      settings =
+        setPort port $
+          setBeforeMainLoop (putStrLn ("Listening on port " ++ show port)) $
+            defaultSettings
+  runSettings settings =<< mkApp
+
+mkApp :: IO Application
+mkApp = return $ serve api server
+
+deriving instance Generic  ActionPD
+instance ToJSON   ActionPD
+instance FromJSON ActionPD
+
+runPlay :: PlayParameters -> Handler PlayResult
+runPlay PlayParameters { player1Action, player2Action } = do
+
+  let strategy = player1Action ::- player2Action ::- Nil
+      next     = play prisonersDilemmaExternal strategy
+
+  (p1, p2) <- liftIO $ extractPayoff next
+
+  let pr = PlayResult { player1Payoff = p1
+                      , player2Payoff = p2 }
+  return pr
+
+strat :: List '[ActionPD, ActionPD]
+strat = Cooperate ::- Cooperate ::- Nil
+
+test :: MonadOptic IO () (Double, Double) (ActionPD, ActionPD) ()
+test = play prisonersDilemmaExternal strat
+
+
+
+{-- Usage:
+extractPayoff test
+
+extractNextState test
+-}
+
+
+
+
+
+prisonersDilemmaExternal :: OpenGame
+                                    (MonadOptic IO)
+                                    (MonadContext IO)
+                                    '[ActionPD, ActionPD]
+                                    '[]
+                                    ()
+                                    (Double, Double)
+                                    (ActionPD, ActionPD)
+                                    ()
 prisonersDilemmaExternal = [opengame|
 
    inputs    :      ;
@@ -49,20 +147,6 @@ extractNextState :: MonadOptic IO () (Double,Double) (ActionPD,ActionPD) () -> I
 extractNextState (MonadOptic v _) = do
   (_,a) <- v ()
   pure a
-
-
-strat = Cooperate ::- Cooperate ::- Nil
-
-test = play prisonersDilemmaExternal strat
-
-
-
-{-- Usage: 
-extractPayoff test
-
-extractNextState test
--}
-
 
 
 
