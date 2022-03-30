223afb1cc1bd157063e478064a2c28f70d274b7f
silky <noonsilk+-noonsilk@gmail.com>
2022-03-30 09:49:11 +0100

    playing around with state

--- src/Examples/ExternalEnvironment/PD.hs
+++ src/Examples/ExternalEnvironment/PD.hs
@@ -14,35 +14,41 @@
 module Examples.ExternalEnvironment.PD where
 
 
-import Engine.Engine hiding (fromLens, fromFunctions)
+import Engine.Engine hiding (fromLens, fromFunctions, state)
 import Preprocessor.Preprocessor
 
 import Engine.ExternalEnvironment
 import Examples.SimultaneousMoves (prisonersDilemmaMatrix,ActionPD(..))
 
--- TODO: Explicit imports!
-import Servant.API
-import Servant
+import Servant                  ((:>), (:<|>)((:<|>)), Server, Handler, Application
+                                , Proxy(..), JSON, Get, Post, ReqBody, serve)
+import Control.Monad.IO.Class   (liftIO)
+import Data.Aeson               (ToJSON, FromJSON)
+import GHC.Generics             (Generic)
 import Network.Wai.Handler.Warp (setPort, setBeforeMainLoop, runSettings, defaultSettings)
-import Data.Aeson
-import GHC.Generics
-import Control.Monad.IO.Class (liftIO)
-
 
 data PlayParameters = PlayParameters
   { player1Action :: ActionPD
   , player2Action :: ActionPD
+  , startingState :: State
+  } deriving (Show, Generic, ToJSON, FromJSON)
+
+
+data State = State
+  { totalPlayer1 :: Double
+  , totalPlayer2 :: Double
   } deriving (Show, Generic, ToJSON, FromJSON)
 
 data PlayResult = PlayResult
-  { player1Payoff :: Double
-  , player2Payoff :: Double
+  { lastPlayer1Payoff  :: Double
+  , lastPlayer2Payoff  :: Double
+  , gameState          :: State
   } deriving (Show, Generic, ToJSON, FromJSON)
 
 -- Using: https://httpie.io/docs/cli/json
 --
 --  http localhost:3000/healthcheck
---  http POST localhost:3000/play player1Action=Cooperate player2Action=Cooperate
+--  http POST localhost:3000/play < json-data/pd-game.json
 --
 type Api = "play" :> ReqBody '[JSON] PlayParameters :> Post '[JSON] PlayResult
            :<|> "healthcheck" :> Get '[JSON] String
@@ -72,15 +78,24 @@ instance ToJSON   ActionPD
 instance FromJSON ActionPD
 
 runPlay :: PlayParameters -> Handler PlayResult
-runPlay PlayParameters { player1Action, player2Action } = do
+runPlay PlayParameters { player1Action, player2Action, startingState } = do
 
   let strategy = player1Action ::- player2Action ::- Nil
       next     = play prisonersDilemmaExternal strategy
 
+  -- TODO: Does this need to be in IO?
   (p1, p2) <- liftIO $ extractPayoff next
 
-  let pr = PlayResult { player1Payoff = p1
-                      , player2Payoff = p2 }
+  -- HACK: We manage state by making the client do it (!)
+  let newState = State { totalPlayer1 = totalPlayer1 startingState + p1
+                       , totalPlayer2 = totalPlayer2 startingState + p2
+                       }
+
+  let pr = PlayResult { lastPlayer1Payoff  = p1
+                      , lastPlayer2Payoff  = p2
+                      , gameState          = newState
+                      }
+
   return pr
 
 strat :: List '[ActionPD, ActionPD]
@@ -89,17 +104,15 @@ strat = Cooperate ::- Cooperate ::- Nil
 test :: MonadOptic IO () (Double, Double) (ActionPD, ActionPD) ()
 test = play prisonersDilemmaExternal strat
 
-
+-- State:
+--  Total Jail Time
+--
 
 {-- Usage:
 extractPayoff test
 
 extractNextState test
 -}
-
-
-
-
 
 prisonersDilemmaExternal :: OpenGame
                                     (MonadOptic IO)
