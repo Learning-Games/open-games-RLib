63897baeefb7110ac0a97cd163c9b6f138be5008
Georgios Karachalias <gdkaracha@gmail.com>
2022-04-01 15:12:44 +0200

    Add Servant API to the RPS game

--- src/Examples/ExternalEnvironment/RockPaperScissors.hs
+++ src/Examples/ExternalEnvironment/RockPaperScissors.hs
@@ -17,12 +17,30 @@ module Examples.ExternalEnvironment.RockPaperScissors where
 import Engine.Engine hiding (fromLens, fromFunctions, state)
 import Preprocessor.Preprocessor
 
+import Control.Monad.IO.Class   (liftIO)
+import Data.Aeson               (ToJSON, FromJSON)
+import GHC.Generics             (Generic)
+import Network.Wai.Handler.Warp (setPort, setBeforeMainLoop, runSettings, defaultSettings)
+import Servant                  ((:>), (:<|>)((:<|>)), Server, Handler, Application
+                                , Proxy(..), JSON, Get, Post, ReqBody, serve)
+
 import Engine.ExternalEnvironment
+import Examples.ExternalEnvironment.Common (extractPayoff)
 
 -- Types
 
 data ActionRPS = Rock | Paper | Scissors
-  deriving (Ord,Eq,Show)
+  deriving (Ord, Eq, Show, Generic, ToJSON, FromJSON)
+
+data PlayParameters = PlayParameters
+  { player1Action :: ActionRPS
+  , player2Action :: ActionRPS
+  } deriving (Show, Generic, ToJSON, FromJSON)
+
+data PlayResult = PlayResult
+  { player1Payoff  :: Double
+  , player2Payoff  :: Double
+  } deriving (Show, Generic, ToJSON, FromJSON)
 
 -- Payoff function
 
@@ -37,7 +55,49 @@ rockPaperScissorsPayoff Scissors Rock = -1
 rockPaperScissorsPayoff Scissors Paper = 1
 rockPaperScissorsPayoff Scissors Scissors = 0
 
+-- Servant API
 
+-- Using: https://httpie.io/docs/cli/json
+--
+--  http localhost:3000/healthcheck
+--  http POST localhost:3000/play < json-data/rps-game.json
+--
+type Api = "play" :> ReqBody '[JSON] PlayParameters :> Post '[JSON] PlayResult
+           :<|> "healthcheck" :> Get '[JSON] String
+
+api :: Proxy Api
+api = Proxy
+
+server :: Server Api
+server =
+  runPlay
+  :<|> return "Ok!"
+
+run :: IO ()
+run = do
+  let port = 3000
+      settings =
+        setPort port $
+          setBeforeMainLoop (putStrLn ("Listening on port " ++ show port)) $
+            defaultSettings
+  runSettings settings =<< mkApp
+
+mkApp :: IO Application
+mkApp = return $ serve api server
+
+runPlay :: PlayParameters -> Handler PlayResult
+runPlay PlayParameters { player1Action, player2Action } = do
+
+  let strategy = player1Action ::- player2Action ::- Nil
+      next     = play rockPaperScissorsExternal strategy
+
+  -- TODO: Does this need to be in IO?
+  (p1, p2) <- liftIO $ extractPayoff next
+
+  let pr = PlayResult { player1Payoff  = p1
+                      , player2Payoff  = p2
+                      }
+  return pr
 
 -- Game
 
